d155a801203e4511f15914470552fd16dd892c65
Alyona <polova_alyona0907@ukr.net>
2023-05-07 18:40:00 +0300

    add to group github

---
+++ src/js/hero.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  loop: true,
+
+  keyboard: {
+    enabled: true,
+  },
+  navigation: {
+    nextEl: '.btn-next',
+    prevEl: '.btn-prev',
+  },
+});
